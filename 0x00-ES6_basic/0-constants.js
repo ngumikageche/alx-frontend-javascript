f89d36dda905288e05cccc1d12250357591bdd13
Joseph Kageche <ngumi98@gmail.com>
2023-09-29 08:50:30 +0300

    this lint1

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,14 +1,12 @@
-// func to instatiate variables using const
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
 
-// func to get last string
 export function getLast() {
   return ' is okay';
 }
-// func to instatiate variables using let
+
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
