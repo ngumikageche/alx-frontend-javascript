66dbfec511c870babdf5ca818b7e8fdf3732c2a9
Joseph Kageche <ngumi98@gmail.com>
2023-09-29 10:29:14 +0300

    fix me here and there

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,10 @@
-export default function getFullResponseFromAPI(success)
-{
-	return new Promise((resolve, reject) => {
-		if (success === true) {
-			// Resolve the Promise a success object
-			resolve({ status: 200, body: 'Success'});
-		} else {
-			reject(new Error('The fake API is not working currently'));
+export default function getFullResponseFromAPI(success){
+  return new Promise((resolve, reject) => {
+    if (success === true) {
+		// Resolve the Promise a success object
+		resolve({ status: 200, body: 'Success'});
+	} else {
+        reject(new Error('The fake API is not working currently'));
 		}
-	});
+  });
 }
