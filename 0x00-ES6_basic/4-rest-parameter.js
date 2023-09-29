9710980f4f1ab23e028857279d333d400f5d6c47
Joseph Kageche <ngumi98@gmail.com>
2023-09-29 09:46:31 +0300

    dont over think

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,9 +1,3 @@
-export default function returmHowManyArguments (...args) {
-	let count = 0
-	for (let i in args)
-	{
-		count = count + 1;
-	}
-	return (count);
-
+export default function returmHowManyArguments(...args) {
+  return args.length;
 }
